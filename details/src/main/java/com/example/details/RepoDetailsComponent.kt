b4e7a3a221a9d31e5484660687a38b90ac6aaec3
Guowei Lv <lvguowei1002@gmail.com>
2020-08-28 23:07:29 +0300

    Repo Details Component

---
+++ details/src/main/java/com/example/details/RepoDetailsComponent.kt
@@ -0,0 +1,40 @@
+package com.example.details
+
+import com.example.appdeps.ApplicationDeps
+import com.example.appdeps.applicationDeps
+import com.example.di.component.getComponent
+import com.example.di.scope.ScreenScope
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Named
+
+@ScreenScope
+@Component(dependencies = [ApplicationDeps::class])
+interface RepoDetailsComponent {
+
+  fun inject(repoDetailsFragment: RepoDetailsFragment)
+
+  @Component.Factory
+  interface Factory {
+    fun create(
+      @BindsInstance @Named("repo_owner") repoOwner: String,
+      @BindsInstance @Named("repo_name") repoName: String,
+      applicationDeps: ApplicationDeps
+    ): RepoDetailsComponent
+  }
+}
+
+fun RepoDetailsFragment.inject() {
+  getComponent {
+    val repoOwner = arguments?.getString("repo_owner")
+      ?: throw NullPointerException("repo_owner must not be null")
+    val repoName = arguments?.getString("repo_name")
+      ?: throw NullPointerException("repo_name must not be null")
+
+    return@getComponent DaggerRepoDetailsComponent.factory().create(
+      repoOwner,
+      repoName,
+      requireContext().applicationDeps()
+    )
+  }.inject(this)
+}
