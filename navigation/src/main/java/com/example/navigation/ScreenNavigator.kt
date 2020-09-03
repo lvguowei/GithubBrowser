3c40beb910d54bb32591997fb7954509e632e3fb
Guowei Lv <lvguowei1002@gmail.com>
2020-09-03 09:17:28 +0300

    Preparing for screen navigation

---
+++ navigation/src/main/java/com/example/navigation/ScreenNavigator.kt
@@ -0,0 +1,26 @@
+package com.example.navigation
+
+import android.annotation.SuppressLint
+import android.app.Activity
+
+sealed class Screen
+
+data class DetailsScreen(val repoOwner: String, val repoName: String) : Screen()
+
+interface ScreenNavigator {
+  fun goToScreen(screen: Screen)
+}
+
+interface NavigationDeps {
+  fun screenNavigator(): ScreenNavigator
+}
+
+const val NAVIGATION_DEPS_SERVICE = "com.sample.githubbrowser.navigationdeps"
+
+@SuppressLint("WrongConstant")
+fun Activity.navigationDeps(): NavigationDeps {
+  val navigationDeps = getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDeps
+    ?: applicationContext.getSystemService(NAVIGATION_DEPS_SERVICE) as? NavigationDeps
+  return navigationDeps
+    ?: throw NullPointerException("Activity must override getSystemService and provide NavigationDeps for service name: $NAVIGATION_DEPS_SERVICE")
+}
